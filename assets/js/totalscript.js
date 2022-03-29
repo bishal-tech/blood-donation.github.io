349dd440be8dcd143571459a45ae14b355bcc33c
Bishal Debsharma <bishal.tech.com@gmail.com>
2022-03-29 21:27:45 +0530

    add done avilable section

--- assets/js/totalscript.js
+++ assets/js/totalscript.js
@@ -1,28 +1,25 @@
 $(document).ready(function () {
+  $(".img-slide").slick({
+    autoplay: true,
+    arrows: false,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: "linear",
+    slidesToShow: 1,
+  });
 
-    $('.img-slider').slick({
-        autoplay: true,
-        arrows: false,
-        dots: true,
-        infinite: true,
-        speed: 500,
-        fade: true,
-        cssEase: 'linear',
-        slidesToShow: 1
-      });
-
-      $('.doners_slide').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-      });
-      $('.articles_slide').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-      });
-                  
-      
+  $(".doners_slide").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+  $(".articles_slide").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
 });

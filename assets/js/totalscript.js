474a55479a002846ccfd50a60c126647031858ff
Bishal Debsharma <bishal.tech.com@gmail.com>
2022-03-30 21:06:07 +0530

    fix the slider issue

--- assets/js/totalscript.js
+++ assets/js/totalscript.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
-  $(".img-slide").slick({
+  $(".img-slider").slick({
     autoplay: true,
     arrows: false,
-    dots: true,
+    dots: false,
     infinite: true,
     speed: 500,
     fade: true,
@@ -10,16 +10,78 @@ $(document).ready(function () {
     slidesToShow: 1,
   });
 
-  $(".doners_slide").slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
+  $(".doners_slider").slick({
+    dots: false,
+    infinite: true,
     autoplay: true,
-    autoplaySpeed: 2000,
+    speed: 300,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
   });
-  $(".articles_slide").slick({
+  $(".articles_slider").slick({
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    speed: 300,
     slidesToShow: 3,
     slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
   });
 });
